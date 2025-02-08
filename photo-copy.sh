88df58c8370ca88d6b2e37701a4ff84b445fefef
Matthew Henderson <matthew.james.henderson@gmail.com>
2025-02-08 19:57:31 +0000

    First lame script.

---
+++ photo-copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+YEAR=2024
+MONTH=12
+
+cp DCIM/109_PANA/*.JPG /Volumes/onetouch/001_Project/Photography\ \($YEAR\)/photo/$YEAR/$MONTH
+cp DCIM/109_PANA/*.JPG /Volumes/Transcend/001_Project/Photography\ \($YEAR\)/photo/$YEAR/$MONTH
+cp DCIM/109_PANA/*.RW2 /Volumes/onetouch/001_Project/Photography\ \($YEAR\)/photo-raw/$YEAR/$MONTH
+cp DCIM/109_PANA/*.RW2 /Volumes/Transcend/001_Project/Photography\ \($YEAR\)/photo-raw/$YEAR/$MONTH
+
