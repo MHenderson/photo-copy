ec218b22580bb2625af8c8cc1be8c378b6d84333
Matthew Henderson <matthew.james.henderson@gmail.com>
2025-02-09 15:40:26 +0000

    Use variables for folder names.

--- photo-copy.sh
+++ photo-copy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-YEAR=2024
-MONTH=12
+YEAR=2025
+MONTH=02
 
-cp DCIM/109_PANA/*.JPG /Volumes/onetouch/001_Project/Photography\ \($YEAR\)/photo/$YEAR/$MONTH
-cp DCIM/109_PANA/*.JPG /Volumes/Transcend/001_Project/Photography\ \($YEAR\)/photo/$YEAR/$MONTH
-cp DCIM/109_PANA/*.RW2 /Volumes/onetouch/001_Project/Photography\ \($YEAR\)/photo-raw/$YEAR/$MONTH
-cp DCIM/109_PANA/*.RW2 /Volumes/Transcend/001_Project/Photography\ \($YEAR\)/photo-raw/$YEAR/$MONTH
+SDCARD=/Volumes/SD001
+
+MAIN_TARGET=/Volumes/onetouch
+BACKUP_TARGET=/Volumes/Transcend
+
+cp $SDCARD/DCIM/109_PANA/*.JPG $MAIN_TARGET/1_Project/Photography$YEAR/photo/$YEAR/$MONTH
+cp $SDCARD/DCIM/109_PANA/*.RW2 $MAIN_TARGET/1_Project/Photography$YEAR/photo-raw/$YEAR/$MONTH
+
+cp $SDCARD/DCIM/109_PANA/*.JPG $BACKUP_TARGET/1_Project/Photography$YEAR/photo/$YEAR/$MONTH
+cp $SDCARD/DCIM/109_PANA/*.RW2 $BACKUP_TARGET/1_Project/Photography$YEAR/photo-raw/$YEAR/$MONTH
 
